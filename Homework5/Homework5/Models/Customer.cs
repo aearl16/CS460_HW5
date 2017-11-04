12280e03323eb8d08e5edffb19c47b1b2ca50c89
Aaron Earl <com_tech12@yahoo.com>
2017-11-04 15:04:39 -0700

    added Customer Model

--- Homework5/Homework5/Models/Customer.cs
+++ Homework5/Homework5/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,45 @@ namespace Homework5.Models
 {
     public class Customer
     {
+        [Required]
         public DateTime DOB { get; set; }
+
+        [Required, StringLength(64)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required, StringLength(64)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required, StringLength(64)]
+        [Display(Name = "Middle Name")]
         public string MiddleName { get; set; }
+
+        [Required, StringLength(256)]
+        [Display(Name = "Address")]
         public string Address { set; get; }
+
+        [Required, StringLength(256)]
+        [Display(Name = "Mailing Address")]
+        public string MailingAddress { get; set; }
+
+        [Display(Name = "Mailing Address")]
+        public string MailingCity { get; set; }
+
+        [Display(Name = "Mailing Zip")]
+        public int MailingZip { get; set; }
+
+        [Required, StringLength(64)]
+        [Display(Name = "City")]
         public string City { get; set; }
+
+        [Required, StringLength(256)]
+        [Display(Name = "State")]
         public string State { get; set; }
+
+        [Required]
+        [Display(Name = "Mailing Address")]
         public int Zipcode { get; set; }
     }
 }
