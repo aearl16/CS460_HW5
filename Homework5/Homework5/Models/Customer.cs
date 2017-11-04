eb10f8efc46c2f01d4ad029d27b82d7a59283d5f
Aaron Earl <com_tech12@yahoo.com>
2017-11-04 14:49:08 -0700

    created branch

---
+++ Homework5/Homework5/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework5.Models
+{
+    public class Customer
+    {
+        public DateTime DOB { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Address { set; get; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
+    }
+}
